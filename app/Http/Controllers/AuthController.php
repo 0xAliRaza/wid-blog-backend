6a73e91f94bf78c47da2724ea145a03de8a1661e
Ali Raza <artanha7@gmail.com>
2020-05-22 05:09:36 +0500

    Setup jwt authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
             return $this->respondWithToken($token);
         }
 
-        return response()->json(['error' => 'Unauthorized'], 401);
+        return response()->json(['error' => 'Username or password is incorrect!'], 401);
     }
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+
+            [
+                'email' => 'admin@gmail.com',
+                'name' => 'Admin',
+                'password' => Hash::make('alimalik'),
+                'role' => 'admin'
+            ],
+            [
+                'email' => 'user@gmail.com',
+                'name' => 'User',
+                'password' => Hash::make('alimalik'),
+                'role' => 'user'
+            ]
+        ]);
     }
 }
